3352b5ba96abcc6d751c9130e82d0631c65b1dc5
RDA <rda@Kirans-Mac-mini.local>
2017-02-23 19:21:33 +0530

    commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,26 +9,24 @@ angular.module('starter.controllers', [])
     $http.get("https://newsapi.org/v1/sources?language=en").success(function(list){
    $scope.newsSources = list.sources;
   })
+  
  }
  $scope.checkItems = function(){
-   
     $scope.CID = [];
     $scope.name = [];
     angular.forEach($scope.newsSources,function(src){
         if(src.checked){
           $scope.CID.push(src.id);
           $scope.name.push(src.name);
+          $scope.color='red';
         }
     });
-    //send CID to server
  };
 $scope.display=function(){
    $window.localStorage.clear();
 var localarray=[];
 var localarrayname=[];
   for(var i=0;i<$scope.CID.length;i++ ){
-   // alert($scope.CID[i])
-  //$window.localStorage[i] = $scope.CID[i];
   localarray[i]=$scope.CID[i];
   localarrayname[i]=$scope.name[i];
   }
@@ -37,10 +35,6 @@ var localarrayname=[];
   localStorage.setItem('dataName',JSON.stringify(localarrayname));
   $scope.outputname=JSON.parse(localStorage.getItem('dataName'));
   console.log($scope.outputname);
-  //var op=JSON.parse(localStorage
-  //$scope.j=JSON.stringify($scope.CID);
-//  alert(JSON.stringify($scope.j))
-//alert($scope.output)
 }
 })
 .controller('newsCntrl',function($scope,$window,$http){
@@ -52,7 +46,6 @@ var localarrayname=[];
    $scope.api=desc;
     $http.get("https://newsapi.org/v1/articles?source="+$scope.api+"&apiKey=850de99fe1d34a3ea6b92fb4e85c540b").then(function(latestNews){
     $scope.latestNewslist = latestNews.data.articles;  
-    //alert(JSON.stringify($scope.latestNewslist))
 })
   }
 })
@@ -60,9 +53,10 @@ var localarrayname=[];
   $scope.a=function(options)
   {
      $http.get("https://newsapi.org/v1/articles?source="+$scope.api+"&sortBy="+options+"&apiKey=850de99fe1d34a3ea6b92fb4e85c540b").then(function(latestNews){
-    $scope.latestNewslist = latestNews.data.articles;  
+    $scope.latestNewslist = latestNews.data.articles; 
   })
-  }
+  
+}
 })
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
